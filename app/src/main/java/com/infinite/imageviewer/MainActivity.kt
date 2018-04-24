826f86cebf508c8a39a3654523e700b65870a1a6
infinite-Zh <zhxuwork@163.com>
2018-04-24 13:18:21 +0800

    fullscreen

--- app/src/main/java/com/infinite/imageviewer/MainActivity.kt
+++ app/src/main/java/com/infinite/imageviewer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.infinite.imageviewer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import layout.ImageViewerFragment
 
 class MainActivity : AppCompatActivity() {
